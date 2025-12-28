c67ad3f186526f265292c163ea0f236beb4e2385
Weebeveloper <aaas.com.com@gmail.com>
2025-12-28 18:12:20 +0200

    deploy yaml

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,42 @@
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [];
 
+// const routes: Routes = [
+//   {
+//     path: '',
+//     redirectTo: 'home',
+//     pathMatch: 'full',
+//   },
+//   {
+//     path: 'home',
+//     component: HomePageComponent,
+//     canActivate: [AuthGuard],
+//   },
+//   {
+//     path: 'details',
+//     component: PersonDetailsPageComponent,
+//     canActivate: [AuthGuard],
+//   },
+//   {
+//     path: 'login',
+//     component: LoginPageComponent,
+//   },
+//   {
+//     path: 'history',
+//     component: HistoryPageComponent,
+//   },
+//   {
+//     path: '**',
+//     redirectTo: 'home',
+//   },
+// ];
+
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers: [{ provide: LocationStrategy, useClass: HashLocationStrategy }],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
